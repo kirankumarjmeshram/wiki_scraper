8c4fdca78cbe1475f3ad3b8e376baea1cc7fd4c5
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2023-10-05 01:26:25 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import LinkScraper from './components/LinkScraper.jsx';
 import Header from './components/Header.jsx'
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <LinkScraper />
+      <Footer/>
     </div>
   );
 }
